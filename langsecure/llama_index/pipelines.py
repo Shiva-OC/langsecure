b6e81b817186c9418a1fb1c78bd5b972d43b28f4
deepio-oc <rajendra.singh@oneconvergence.com>
2024-12-16 04:58:54 +0100

    send violation message on stream

--- langsecure/llama_index/pipelines.py
+++ langsecure/llama_index/pipelines.py
@@ -1,15 +1,14 @@
 """Arg pack components."""
 
 from typing import Any, Callable, Dict, Optional
-import traceback
-
+import concurrent.futures
 from llama_index.core.base.query_pipeline.query import (
     InputKeys,
     OutputKeys,
     QueryComponent,
 )
 from llama_index.core.bridge.pydantic import Field
-
+import asyncio
 
 class StopComponent(QueryComponent):
     """Stop  component.
@@ -19,7 +18,7 @@ class StopComponent(QueryComponent):
     """
 
     message: str = "Pipeline execution terminated."
-
+    callback_manager: Any = None
     def _validate_component_inputs(self, input: Dict[str, Any]) -> Dict[str, Any]:
         """Validate component inputs during run_component."""
         raise NotImplementedError
@@ -37,10 +36,48 @@ class StopComponent(QueryComponent):
 
     def set_callback_manager(self, callback_manager: Any) -> None:
         """Set callback manager."""
+        self.callback_manager = callback_manager
 
     def _run_component(self, **kwargs: Any) -> Any:
         """Run component."""
         msg = kwargs['message']
+        if self.callback_manager:
+            for handler in self.callback_manager.handlers:
+                if hasattr(handler, "IDENTIFIER"):
+                    if handler.IDENTIFIER == "streaming_handler":
+                        streaming_handler = handler
+            async def streamer(response):
+                if streaming_handler and streaming_handler._target_streamer:
+                    streaming_handler._target_streamer.websocket_streamer.queue.put_nowait({})
+
+                await streaming_handler.on_stream_start()
+                await streaming_handler.on_new_token(response)
+                await streaming_handler.on_stream_end()
+
+            async def async_stream(prompt):
+                response = await streamer(prompt)
+                return response
+
+            def stream_sync_to_async(prompt):
+                loop = asyncio.new_event_loop()
+                asyncio.set_event_loop(loop)
+
+                result = loop.run_until_complete(async_stream(prompt))
+                return result
+            async def wait_for_future():
+                result = await loop.run_in_executor(None, future.result)
+                return result
+            executor = concurrent.futures.ThreadPoolExecutor(max_workers=1)
+            future = executor.submit(stream_sync_to_async, msg)
+            try:
+                loop = asyncio.get_event_loop()
+            except RuntimeError as e:
+                if str(e).startswith('There is no current event loop in thread'):
+                    loop = asyncio.new_event_loop()
+                    asyncio.set_event_loop(loop)
+                else:
+                    raise
+            loop.run_until_complete(wait_for_future())
         return {"output": msg}
 
     async def _arun_component(self, **kwargs: Any) -> Any:
